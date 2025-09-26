726f603140a3f82f81732c8926205a714c00f9bb
Yoimer <yoimerandresmartines342@gmail.com>
2025-09-26 17:24:07 -0500

    agrega archivo index para exportación de servicios

---
+++ backend/services/index.js
@@ -0,0 +1,9 @@
+const ActivityService = require('./ActivityService');
+const UserService = require('./UserService');
+const FavoriteService = require('./FavoriteService');
+
+module.exports = {
+  ActivityService,
+  UserService,
+  FavoriteService
+};
