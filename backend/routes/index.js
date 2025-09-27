090e04c8e0e00aa5170da97830d417116161a413
Yoimer <yoimerandresmartines342@gmail.com>
2025-09-27 15:59:24 -0500

    agrega archivo index para exportación de rutas

---
+++ backend/routes/index.js
@@ -0,0 +1,9 @@
+const authRoutes = require('./authRoutes');
+const activityRoutes = require('./activityRoutes');
+const favoriteRoutes = require('./favoriteRoutes');
+
+module.exports = {
+  authRoutes,
+  activityRoutes,
+  favoriteRoutes
+};
